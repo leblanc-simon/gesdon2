168e5be4b4827f14e373097d24a686649dd7f2b6
victortoulouse <victor@framasoft.org>
2015-02-09 13:39:36 +0100

    En cours de factorisation. Contrôleurs et vues spécifiques aux entités supprimés. Ajout, lecture et suppression fonctionnent, édition ne fonctionne pas encore.

--- src/Gesdon2Bundle/Controller/DefaultController.php
+++ src/Gesdon2Bundle/Controller/DefaultController.php
@@ -3,17 +3,27 @@
 namespace Gesdon2Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * Menu des entités modifiables.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
+        // tableau de entités éditables
+        $entities = array("Donateur", "Adresse", "Don");
 
-        $routes = array("donateur", "adresse", "don");
         return $this->render('Gesdon2Bundle:Default:index.html.twig',
             array(
-                'routes' => $routes
+                'entities' => $entities
             )
         );
     }
@@ -21,30 +31,27 @@ class DefaultController extends Controller
     /**
      * Afficher la liste des instances de l'entité passée en paramètre.
      *
-     * @param $nomEntite
+     * @param String $entity
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function listeAction($nomEntite)
+    public function listAction($entity)
     {
-        // invoquer un Entity Manager pour persister les données
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
 
         // retrouver la liste des instances de l'entité
-        $nomBundle = "Gesdon2Bundle:";
-        $entite = $em->getRepository($nomBundle.$nomEntite);
-        $instances = $entite->findAll();
+        $bundle = "Gesdon2Bundle:";
+        $instances = $em->getRepository($bundle.$entity)->findAll();
 
         // retrouver les attributs de l'entité
-        $colonnes = $em ->getClassMetadata($nomBundle.$nomEntite)
-                        ->getColumnNames();
+        $fields = $em ->getClassMetadata($bundle.$entity)->getFieldNames();
 
-        // générer la page à retourner à partir du template twig "index"
+        // générer la page à retourner à partir du template twig "list"
         // en passant la liste des instances de l'entité
-        return $this->render('Gesdon2Bundle:Default:liste.html.twig',
+        return $this->render('Gesdon2Bundle:Default:list.html.twig',
             array(
-                'instances' => $instances,
-                'classe'    => $nomEntite,
-                'colonnes'  => $colonnes
+                'instances'=> $instances,
+                'entity'  => $entity,
+                'fields'  => $fields
             )
         );
     }
@@ -53,26 +60,30 @@ class DefaultController extends Controller
      * Générer le formulaire de création d'instance de l'entité passée en paramètre.
      *
      * @param Request $request
-     * @param $classe
+     * @param String $entity
      * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/new", name="new")
+     * @Method("GET")
+     * @Template("Gesdon2Bundle:Default:ajouter.html.twig")
      */
-    public function ajouterAction(Request $request, $classe)
+    public function newAction(Request $request, $entity)
     {
         // invoquer un Entity Manager pour persister les données
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
 
         // concaténer le namespace et le nom de la classe
-        // TODO vérifier s'il n'y a pas un meilleur procédé
-        $namespace = 'Gesdon2Bundle\\Entity\\';
-        $namespaceClasse = $namespace . $classe;
+        $namespaceClasse = 'Gesdon2Bundle\\Entity\\' . $entity;
         // créer l'objet
-        $objet = new $namespaceClasse;
+        $entityObject = new $namespaceClasse;
 
     	// créer l'objet formulaire à partir du type
-        // TODO idem, vérifier s'il n'y a pas un meilleur procédé
-        $nomType = 'Gesdon2Bundle\\Form\\' . $classe . "Type";
-        $type = new $nomType;
-        $form = $this->createForm($type, $objet);
+        $type = 'Gesdon2Bundle\\Form\\' . $entity . "Type";
+        $typeObject = new $type;
+        $form = $this->createForm($typeObject, $entityObject);
+
+        // ajouter le bouton d'envoi
+        $form->add('submit', 'submit', array('label' => 'Créer'));
 
         // traiter la soumission du formulaire
         $form->handleRequest($request);
@@ -80,39 +91,225 @@ class DefaultController extends Controller
         // si le formulaire est validé
         if ($form->isValid()) {
             // récupérer les données du formulaire
-            $objet = $form->getData();
+            $entityObject = $form->getData();
 
             // persister l'objet
             //TODO traiter les erreurs SQL
-            $em->persist($objet);
+            $em->persist($entityObject);
             $em->flush();
 
-            //return $this->redirect($this->generateUrl('gesdon2_cree',array('nomEntite'=>$classe)));
-            return $this->render('Gesdon2Bundle:Default:cree.html.twig',
-                array(
-                    'nomEntite' => $classe
-                )
-            );
+            return $this->redirect($this->generateUrl('show', array(
+                'entity' => $entity,
+                'id' => $entityObject->getId()
+            )));
         }
 
         // retourner le formulaire d'ajout
         // le nom de la classe et le formulaire correspondant au type d'objet sont passés en paramètre
-        return $this->render('Gesdon2Bundle:Default:ajouter.html.twig',
+        return $this->render('Gesdon2Bundle:Default:new.html.twig',
             array
             (
-                'objet' => $classe,
+                'entity' => $entity,
                 'form'  => $form->createView()
             )
         );
     }
 
+    /**
+     * Trouver et afficher une instance.
+     *
+     * @Route("/{id}", name="show")
+     * @Method("GET")
+     * @Template()
+     *
+     * @param $entity
+     * @param $id
+     * @return array
+     */
+    public function showAction($entity, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $instance = $em->getRepository('Gesdon2Bundle:'.$entity)->find($id);
+
+        if (!$instance) {
+            throw $this->createNotFoundException('Unable to find ' . $entity . ' instance.');
+        }
+
+        $deleteForm = $this->createDeleteForm($entity, $id);
+
+        return array(
+            'entity'      => $entity,
+            'instance'    => $instance,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
 
     /**
-     * Afficher la page de notification de création de l'instance.
+     * Afficher un formulaire pour modifier un instance.
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param String $entity
+     * @param Integer $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/{id}/edit", name="edit")
+     * @Method("GET")
+     * @Template()
      */
-    public function creeAction(){
-    	return $this->render('Gesdon2Bundle:Default:cree.html.twig');
+    public function editAction($entity,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // retrouver l'instance de l'entité avec l'id
+        $instance = $em->getRepository('Gesdon2Bundle:'.$entity)->find($id);
+
+        if (!$instance) {
+            throw $this->createNotFoundException('Unable to find '.$entity.' instance.');
+        }
+
+        $editForm = $this->createEditForm($entity, $instance);
+        $deleteForm = $this->createDeleteForm($entity, $id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
     }
+
+    /**
+     * Créer un formulaire pour modifier une instance.
+     *
+     * @param String $entity
+     * @param $instance
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createEditForm($entity, $instance)
+    {
+        // créer l'objet type à partir du nom
+        $type = 'Gesdon2Bundle\\Form\\' . $entity . "Type";
+        $typeObject = new $type;
+
+        // créer le formulaire
+        $form = $this->createForm(
+            $typeObject,
+            $instance,
+            array(
+                'action' => $this->generateUrl(
+                    'update',
+                    array(
+                        'entity' => $entity,
+                        'id' => $instance->getId()
+                    )
+                ),
+                'method' => 'PUT',
+            )
+        );
+
+        $form->add('submit', 'submit', array('label' => 'Modifier'));
+
+        return $form;
+    }
+
+    /**
+     * Modifier une instance.
+     *
+     * @param Request $request
+     * @param String $entity
+     * @param Integer $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/{entity}/{id}", name="update")
+     * @Method("PUT")
+     * @Template("Gesdon2Bundle:Default:edit.html.twig")
+     */
+    public function updateAction(Request $request, $entity, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        // retrouver l'instance par son ID
+        $instance = $em->getRepository('Gesdon2Bundle:'.$entity)->find($id);
+        // si l'instance est introuvable, revoyer un message d'erreur
+        if (!$instance) {
+            throw $this->createNotFoundException('Unable to find '.$entity.' instance.');
+        }
+
+        $deleteForm = $this->createDeleteForm($entity, $instance);
+        $editForm = $this->createEditForm($entity, $instance);
+        $editForm->handleRequest($request);
+
+        // si le formulaire est validé...
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('edit', array(
+                'entity'=>$entity,
+                'id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Supprimer une instance.
+     *
+     * @param Request $request
+     * @param String $entity
+     * @param Integer $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/{entity}/{id}", name="delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $entity, $id)
+    {
+        $form = $this->createDeleteForm($entity, $id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            // retrouver l'instance grâce à son ID
+            $instance = $em->getRepository('Gesdon2Bundle:'.$entity)->find($id);
+
+            // si l'instance est introuvable, renvoyer un message d'erreur
+            if (!$instance) {
+                throw $this->createNotFoundException('Unable to find '.$entity.' instance.');
+            }
+
+            // supprimer l'instance
+            $em->remove($instance);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('list', array('entity'=> $entity)));
+    }
+
+    /**
+     * Créer un formulaire pour supprimer une instance.
+     *
+     * @param String $entity
+     * @param Integer $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($entity, $id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('delete', array(
+                'entity' => $entity,
+                'id' => $id)
+            ))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Supprimer'))
+            ->getForm()
+            ;
+    }
+
 }
